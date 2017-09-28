97dbfd0df098e3d26c1b4cb8c9cd6a1087542e2d
Joe Witt <joewitt@K4455.local>
2017-09-28 14:26:52 -0400

    code cleanup and removal of useless data structures.

--- src/main/java/oktareport/UniqueUsers.java
+++ src/main/java/oktareport/UniqueUsers.java
@@ -11,14 +11,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * Represents unique login users.  This class can probably be simplified and rely on the csv output to
- * group logins by source.  LoginsByIdpSource hashmap is useless at this point, since it was only used for
- * reporting in the console reporting.  Revisit based on feedback.
+ * Represents unique login users.
  */
 public class UniqueUsers {
 
-    private HashMap<String, BCBSALoginUser> loginUsers = new HashMap<String, BCBSALoginUser>();
-    private HashMap<String, HashMap<String, Integer>> loginsByIdpSource = new HashMap<String, HashMap<String, Integer>>();
+    private HashMap<String, LoginUser> loginUsers = new HashMap<String, LoginUser>();
 
     private static final Logger logger = LogManager.getLogger(UniqueUsers.class);
 
@@ -43,38 +40,19 @@ public class UniqueUsers {
         if(loginUsers.containsKey(userId)) {
             loginUsers.get(userId).addAuth(requestId);
         } else {
-            BCBSALoginUser user = new BCBSALoginUser(userId, requestId, "");
+            LoginUser user = new LoginUser(userId, requestId, "");
             loginUsers.put(userId, user);
         }
     }
 
     private void addSource(String userId, String requestId, String idpSource) {
         loginUsers.get(userId).addIdpSource(requestId, idpSource); //must exist
-        if(loginsByIdpSource.containsKey(idpSource)) {
-            if (loginsByIdpSource.get(idpSource).containsKey(userId)) {
-                int value = loginsByIdpSource.get(idpSource).get(userId).intValue();
-                loginsByIdpSource.get(idpSource).put(userId, value++);
-            } else {
-                loginsByIdpSource.get(idpSource).put(userId, 1);
-            }
-        } else {
-            HashMap<String, Integer> userMap = new HashMap<String, Integer>();
-            userMap.put(userId, 1);
-            loginsByIdpSource.put(idpSource, userMap);
-        }
     }
 
     public static int getUniqueAuthCount() {
         return uniqueUsers.loginUsers.size();
     }
 
-    public static HashMap<String, Integer> getUniqueByIdpSources() {
-        HashMap<String, Integer> counts = new HashMap<String, Integer>();
-        uniqueUsers.loginsByIdpSource.forEach((source, userMap) -> {
-            counts.put(source, userMap.size());
-        });
-        return counts;
-    }
 
     public static void getCSV() {
         try {
@@ -94,13 +72,13 @@ public class UniqueUsers {
         }
     }
 
-    private class BCBSALoginUser {
+    private class LoginUser {
         private String userId;
         private int authCount;
         private String idpSource = "local";
         private HashMap<String, String> requestIDPMap = new HashMap<String, String>(); //Look at memory consumption on large dataloads this could be removed.
 
-        public BCBSALoginUser(String userId, String requestId, String idpSource) {
+        public LoginUser(String userId, String requestId, String idpSource) {
             this.userId = userId;
             this.requestIDPMap.put(requestId, "");
             this.authCount = 1;
@@ -134,7 +112,7 @@ public class UniqueUsers {
 
         @Override
         public boolean equals(Object user) {
-            return this.userId.equals(((BCBSALoginUser)user).userId)?true:false;
+            return this.userId.equals(((LoginUser)user).userId)?true:false;
         }
 
         @Override
